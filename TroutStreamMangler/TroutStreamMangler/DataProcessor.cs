b6a7dbb52f5ed9203393ca0e4eef9281b7c1b457
standersnerdery <standers@nerdery.com>
2015-01-19 15:36:23 -0600

    Remove Class1.cs and add DataProcessors

---
+++ TroutStreamMangler/TroutStreamMangler/DataProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TroutStreamMangler
+{
+    public abstract class DataProcessor : IDataProcessor
+    {
+        protected readonly IDatabaseImporter _importer;
+        protected readonly IDatabaseExporter _exporter;
+
+        protected DataProcessor(IDatabaseImporter importer, IDatabaseExporter exporter)
+        {
+            _importer = importer;
+            _exporter = exporter;
+        }
+
+        protected virtual void PreProcess()
+        {
+            
+        }
+
+        protected virtual void PostProcess()
+        {
+
+        }
+
+        public void Process()
+        {
+            PreProcess();
+
+            PostProcess();
+        }
+    }
+}
